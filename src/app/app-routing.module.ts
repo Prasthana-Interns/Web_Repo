8e730b7c2e68f1da8c8aede65e3755ea697e6463
chaitanya <chaitanya.balanagu341@gmail.com>
2022-12-01 15:00:22 +0530

    Auth module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { ResetPasswordComponent } from './reset-password/reset-password.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
 import { AdminComponent } from './admin/admin.component';
 import { DevicesComponent } from './devices/devices.component';
 import { AddDeviceComponent } from './add-device/add-device.component';
 import { ApprovalsComponent } from './approvals/approvals.component';
 
 
-const routes: Routes = [
-                        {path:'login',component:LoginComponent},
-                        {path:'reset',component:ResetPasswordComponent},
-                        {path:'signup',component:SignUpComponent},
+const routes: Routes = [                       
                         {path:'admin',component:AdminComponent,
                           children:[
                             {path:'devices',component:DevicesComponent,
                             children:[{path:'add-device',component:AddDeviceComponent}]},
                             {path:'approvals',component:ApprovalsComponent},
                           ]
-                        } 
+                        }, 
+                        {path:"auth",loadChildren:()=> import('./auth/auth.module').then((m)=>m.AuthModule)},
                       ];
 
 @NgModule({
