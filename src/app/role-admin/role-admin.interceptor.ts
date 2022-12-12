8fd326180a41aa433d2506d1eb11a0bba1d4749e
SwarnaMudhiraj <swarnamamindla1@gmail.com>
2022-12-12 16:06:21 +0530

    resolved conflicts in employee-popup component

--- src/app/role-admin/role-admin.interceptor.ts
+++ src/app/role-admin/role-admin.interceptor.ts
@@ -25,32 +25,3 @@ export class RoleAdminInterceptor implements HttpInterceptor {
     return next.handle(token);
   }
 }
-
-// import { Injectable} from '@angular/core';
-// import {
-//   HttpRequest,
-//   HttpHandler,
-//   HttpEvent,
-//   HttpInterceptor
-// } from '@angular/common/http';
-// import { Observable } from 'rxjs';
-// import { HttpRequestService } from './http-request.service';
-
-// @Injectable()
-// export class RoleAdminInterceptor implements HttpInterceptor {
-
-//   constructor(public httpService:HttpRequestService) {}
-
-//   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-
-//     let token= request.clone(
-//       {
-//       setHeaders: {
-//            Authorization:`${this.httpService.getToken()}`
-//          }
-//        }
-//       )
-//     return next.handle(token);
-//   }
-// }
- 
