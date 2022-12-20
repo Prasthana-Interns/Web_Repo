ed46f5f89dae7fb837c49384a21e808c307aa431
Mayur <mayurchowhan08@gmail.com>
2022-12-20 11:24:55 +0530

    adminLogoChanged

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -5,22 +5,21 @@ import { LoginComponent } from './login/login.component';
 import { ResetPasswordComponent } from './reset-password/reset-password.component';
 import { ReactiveFormsModule,FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { FooterComponent } from './footer/footer.component';
+import { CoreModule } from '../core/core.module';
 
 @NgModule({
   declarations: [
     LoginComponent,
     ResetPasswordComponent,
-    FooterComponent
   ],
   imports: [
     CommonModule,
     AuthRoutingModule,
     ReactiveFormsModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    CoreModule
   ],
-  exports:[FooterComponent]
 
 })
 export class AuthModule { }
