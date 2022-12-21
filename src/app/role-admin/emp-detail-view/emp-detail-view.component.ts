5e3c7487f88a2eacc1be7ea550a3b56cc45c6743
SwarnaMudhiraj <swarnamamindla1@gmail.com>
2022-12-21 17:52:26 +0530

    final changes

--- src/app/role-admin/emp-detail-view/emp-detail-view.component.ts
+++ src/app/role-admin/emp-detail-view/emp-detail-view.component.ts
@@ -10,75 +10,67 @@ import { EmployeeListComponent } from '../employee-list/employee-list.component'
   styleUrls: ['./emp-detail-view.component.css']
 })
 export class EmpDetailViewComponent  implements OnInit{
-  // public empDetailForm: any = true;
-
   public employeeId: any;
   public employeeData: any;
   public _id: any;
   role: any
-  
   hasDevicesForm = false;
   noDevicesAssigned = false;
   
-  constructor( private activatedRoute:ActivatedRoute,private httpService:HttpRequestService,private router:Router,private confirmService:NgConfirmService) { }
-  
-  ngOnInit() {
-    this.activatedRoute.paramMap.subscribe((params: ParamMap) => {
-    let id = params.get('id');
-    this.employeeId = id;})
-    this.getEmployeeById()
-   
-  
-  }
-  getEmployeeById() {
-    this.httpService.get(`users/${this.employeeId}`).subscribe(response => {
-     
-      if (response) {
-         this.employeeData = response;
-        console.log(response)
-        if (this.employeeData.devices.length == 0) {
-          this.noDevicesAssigned = true
-        }
-        if (this.employeeData?.user_roles?.includes('Admin')) {
-          this.role = 'Admin'
-        }
-        else {
-          this.role = 'Employee'
-        }
-      }
-      })
-  }
-  deleteEmployee() {
-    this.confirmService.showConfirm("Are you sure to delete", () => { 
-    this.httpService.delete(`users`,this.employeeId).subscribe(res => {
-      this.httpService.get(`users`).subscribe(res => { })
-    })
-    this.router.navigate(['/admin/admin/employees'])
-   }, () => {}) 
-  } 
-  addDevice() {
-    this.hasDevicesForm=true
-    this._id = this.employeeId;
-  this.noDevicesAssigned =false;
+constructor( private activatedRoute:ActivatedRoute,private httpService:HttpRequestService,private router:Router,private confirmService:NgConfirmService) { }
 
-  }
-  unassignDevicesList(data: any) {
-    this.hasDevicesForm = data;
-    this.getEmployeeById();
-  }
-  unAssignDevice_delete(devId: any) {
-    this.confirmService.showConfirm("Are you sure to delete",() => {
-    const body = {
-          "device":
-          {
-            "user_id": null
-          }
-        }
-    this.httpService.put(`devices/${devId}`, body).subscribe(res => {
-    this.getEmployeeById();
-    })
-    },() =>{})
- }
-  }
+ngOnInit() {
+  this.activatedRoute.paramMap.subscribe((params: ParamMap) => {
+  let id = params.get('id');
+  this.employeeId = id;})
+  this.getEmployeeById()
+}
+getEmployeeById() {
+  this.httpService.get(`users/${this.employeeId}`).subscribe(response => {
+  if (response) {
+    this.employeeData = response;
+    if (this.employeeData.devices.length == 0) {
+    this.noDevicesAssigned = true
+    }
+    if (this.employeeData?.user_roles?.includes('Admin')) {
+    this.role = 'Admin'
+    }
+   else {
+    this.role = 'Employee'
+    }
+    }
+  })
+}
+deleteEmployee() {
+  this.confirmService.showConfirm("Are you sure to delete", () => { 
+  this.httpService.delete(`users`,this.employeeId).subscribe(res => {
+  this.httpService.get(`users`).subscribe(res => { })
+  })
+  this.router.navigate(['/admin/admin/employees'])
+  }, () => {}) 
+} 
+addDevice() {
+  this.hasDevicesForm=true
+  this._id = this.employeeId;
+  this.noDevicesAssigned =false;
+}
+unassignDevicesList(data: any) {
+  this.hasDevicesForm = data;
+  this.getEmployeeById();
+}
+unAssignDevice_delete(devId: any) {
+  this.confirmService.showConfirm("Are you sure to delete",() => {
+  const body = {
+      "device":
+      {
+        "user_id": null
+      }
+    }
+  this.httpService.put(`devices/${devId}`, body).subscribe(res => {
+  this.getEmployeeById();
+  })
+  },() =>{})
+}
+}
 
 
