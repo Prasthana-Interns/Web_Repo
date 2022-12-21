34d40c6374f093be53eb585f9bdc76ee8d447597
Mayur <mayurchowhan08@gmail.com>
2022-12-21 15:29:30 +0530

    approvals30

--- src/app/role-admin/devices/devices.component.ts
+++ src/app/role-admin/devices/devices.component.ts
@@ -46,7 +46,6 @@ export class DevicesComponent implements OnInit {
     this.confirmService.showConfirm("Are you sure want to Delete ?",
      () => {
       this.http.delete(`devices`,id).subscribe((res)=>{
-        console.log(res)
         this.getAllDevices();
       })
     },
